4caab51c7f8280fa8cbfe64a8674d5349047ea85
Leo <leonardocastiglione@hotmail.com>
2023-01-22 23:50:58 -0300

    new code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
+const apiURL="https://api.giphy.com/v1/gifs/search?api_key=2eFrEajT4zkWEXKTGgbHL6qawSHMIzOi&q=software&limit=10&offset=0&rating=g&lang=en"
+
 function App() {
+  const [gifs, setGifs]=useState([])
+
+  useEffect(()=>{
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(response=>{
+      const {data}=response
+      const gifs=data.map(image=>image.images.downsized_medium.url)
+      setGifs(gifs)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(singleGif => <img src={singleGif}/>)
+        }
+        {/* <button onClick={()=>setGifs(DIFFERENTS_GIFS)}> Cambiar GIFs </button> */}
+      </section>
     </div>
   );
 }
