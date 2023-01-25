df4ae93682a941568f070dd36726f3e69d0d4d72
lcastiglione <leonardocastiglione@hotmail.com>
2023-01-25 11:58:53 -0300

    Lazy load

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,7 +2,7 @@
 import getTrendingTerms from "services/getTrendingTermsService";
 import Category from "components/Category";
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([]);
 
   useEffect(() => {
@@ -10,4 +10,28 @@ export default function TrendingSearches() {
   }, []);
 
   return <Category name="Tendencias" options={trends} />;
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const onChange = (entries) => {
+      const el = entries[0];
+      if (el.isIntersecting) {
+        setShow(true);
+      }
+    };
+
+    /*
+    IntersectionObserver provee una vía asíncrona para observar cambios en la intersección de un elemento con un elemento superior.
+    */
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: "100px",
+    });
+
+    observer.observe(document.getElementById("LazyTrending"));
+  }, []);
+
+  return <div id="LazyTrending">{show ? <TrendingSearches /> : null}</div>;
 }
